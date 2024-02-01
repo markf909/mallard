25b9e43d87465d7de70a684b5249ea07f5def935
markf909 <156904810+markf909@users.noreply.github.com>
2024-02-01 22:32:29 +0000

    Add files via upload

---
+++ star wars.ps1
@@ -0,0 +1,10 @@
+Add-Type -AssemblyName System.Windows.Forms
+
+# Display a message box with OK button
+$result = [System.Windows.Forms.MessageBox]::Show('Click OK to open a webpage.', 'Message Box', 'OK', 'Information')
+
+# Check if the user clicked OK (DialogResult.OK)
+if ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    # Open a webpage using the default web browser
+    Start-Process 'https://www.youtube.com/watch?v=C8ZvsFCxxCU'
+}
